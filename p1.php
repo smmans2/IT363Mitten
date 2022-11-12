a75ae8c70d2ce1415b1d9167bd3e5f9eaf1c43a7
SRDooman <scottdooman@gmail.com>
2022-11-11 20:00:49 -0600

    table update

--- p1.php
+++ p1.php
@@ -42,7 +42,23 @@
 			color: white;
 			height: 1500px;
 		}
-		
+       .title{
+        text-align: center;
+        padding-top: 20px;
+        padding-bottom: 15px;
+        font-size: 20px;
+       }
+		#main-zone
+		{
+			width:100%;
+			height:800px;
+			padding-top:100px;
+			font-size: 23px;
+			float: center;
+			background-color: #CFDDC8;
+			color: black;
+			text-shadow: none;
+		}
 		* {box-sizing: border-box;}
 
 		input[type=text], select, textarea 
@@ -75,7 +91,8 @@
         table, td, th 
         {  
         border: 1px solid #ddd;
-        text-align: left;
+		align: center;
+		background-color: #CFDDC8;
         }
 		.box 
         {
@@ -91,33 +108,38 @@
 
         .clearfix::after 
         {
-            padding-top: 100px;
+            padding-top: 400px;
 			font-size: 23px;
+			padding-left: 5%;
+			padding-right:5%;
 			background-color: #CFDDC8;
 			color: black;
 			text-shadow: none;
             content: "";
             clear: both;
             display: table;
+			float:center;
+	
         }
         .card {
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-        width: 33.33%;
-        padding: 50px;
-        padding-top: 100px;   
+        box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.2);
+        width: 400px;
+		height: 300px;  
+		padding-top:3%;
         font-size: 23px;
-		background-color: #CFDDC8;
+		background-color: white;
 		color: black;
 	    text-shadow: none;
         text-align: center;
         font-family: arial;
         float: left;
+		
 
         }
 
         .price 
         {
-            color: grey;
+            color: black;
             font-size: 22px;
         }
         .card button 
@@ -137,6 +159,7 @@
         {
             opacity: 0.7;
         }
+
 		
 			</style> 
 		
@@ -149,23 +172,83 @@
             <a href="Products.php"style="background:white; font-size:23px;color: #18453B;">Products</a>
             <a href="Review.php">Reviews</a>
       	</div>
-        <div class="clear-fix">
+        <div class ="title">
+<h1> Products</h1>
+	</div>
+<?php
+$conn = mysqli_connect("localhost", "admin", "admin","mitten");
+$i=1001;
+$length=19;
+if (mysqli_connect_errno())
+{
+    echo "Connection Error" . mysqli_connect_error();
+}
+$sql = "SELECT * FROM products WHERE productID = $i";
+$result = mysqli_query($conn,$sql);
+                
+    if($result->num_rows > 0)
+    {
+        for($j=0; $j<=$length; $j++)
+        {
+            ?>
+            <table>
+                <tr>
+                    <?php
+            $sql = "SELECT * FROM products WHERE productID = $i";
+            $result = mysqli_query($conn,$sql);
+            $row = $result->fetch_assoc();
+            ?>
+            <div class="clear-fix">
             <div class="card">
-                <p>Test<p>
+                <img src="<?php echo $row["productPicture"]?>" alt="Picture" style="width:30%">
+                <h2><?php echo $row["productDescription"]?></h2>
+                <p><?php echo $row["productPrice"]?></p>
             </div>
+            </div>
+        </tr>
+            <?php
+            $i++;
+            $j++;
+
+
+            $sql = "SELECT * FROM products WHERE productID = $i";
+            $result = mysqli_query($conn,$sql);
+            $row = $result->fetch_assoc();
+            ?>
+            <div class="clear-fix">
             <div class="card">
-                <p>Test<p>
+                <img src="<?php echo $row["productPicture"]?>" alt="Picture" style="width:30%">
+                <h2><?php echo $row["productDescription"]?></h2>
+                <p class="price"><?php echo $row["productPrice"]?></p>
             </div>
+            </div>
+            <?php
+            $i++;
+            $j++;
+
+            $sql = "SELECT * FROM products WHERE productID = $i";
+            $result = mysqli_query($conn,$sql);
+            $row = $result->fetch_assoc();
+            ?>
+            <div class="clear-fix">
             <div class="card">
-                <p>Test<p>
+                <img src="<?php echo $row["productPicture"]?>" alt="Picture" style="width:30%">
+                <h2><?php echo $row["productDescription"]?></h2>
+                <p class="price"><?php echo $row["productPrice"]?></p>
             </div>
-        </div>
-    
-    
+            </div>
+            <?php
+            $i++;
+            $j++;
 
 
+        }
+        
+            
+        
+    }   
+        ?>
 
-
-
+</table>
 </body>
 </html>
